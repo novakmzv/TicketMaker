ce19d28b353a0e44ecc0062f85a288d981877b6b
novak <novakmzv@gmail.com>
2025-08-29 12:31:39 -0500

    feat: implemented migrations 🗃️

---
+++ database/migrations/2025_08_29_172852_create_tickets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('client_name');
+            $table->decimal('final_price', 8, 2);
+            $table->string('ticket_code')->unique();
+            $table->foreignId('client_type_id')->constrained();
+            $table->foreignId('game_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
